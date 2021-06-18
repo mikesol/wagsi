56b9c7226961db52b13f4ad49c985645c10369b9
Mike Solomon <mike@meeshkan.com>
2021-06-18 11:37:01 +0300

    Graph

--- src/EngineTemplate.purs
+++ src/EngineTemplate.purs
@@ -1,8 +1,8 @@
 module EngineTemplate where
 
 import Prelude
-
 import Control.Comonad (extract)
+import Control.Monad.Indexed.Qualified as Ix
 import Data.Either (Either(..))
 import Data.Tuple.Nested (type (/\))
 import Hack (Evt(..), Extern, Wag(..))
@@ -11,42 +11,54 @@ import WAGS.Control.Functions.Validated (ibranch)
 import WAGS.Control.Indexed (IxWAG(..))
 import WAGS.Control.Types (Scene, WAG)
 import WAGS.Graph.AudioUnit (TConstant, TSpeaker)
+import WAGS.Patch (class Patch, ipatch)
 import WAGS.Run (RunAudio, RunEngine)
 import WAGS.Validation (class GraphIsRenderable)
 
 type Graph
-  = { speaker :: TSpeaker /\ { toSpeaker :: Unit }
+  = ( speaker :: TSpeaker /\ { toSpeaker :: Unit }
     , toSpeaker :: TConstant /\ {}
-    }
+    )
 
-type Control = Unit
+type Control
+  = Unit
 
 cont' ::
   forall newGraph newControl proof.
   GraphIsRenderable newGraph =>
-  (Control -> IxWAG RunAudio RunEngine proof Unit Graph {|newGraph} newControl) ->
-  (forall newProof. Extern -> newControl -> IxWAG RunAudio RunEngine newProof Unit {|newGraph} {|newGraph} newControl) ->
-  WAG RunAudio RunEngine proof Unit Graph Control ->
+  Patch Graph newGraph =>
+  (Control -> IxWAG RunAudio RunEngine proof Unit { | newGraph } { | newGraph } newControl) ->
+  (forall newProof. Extern -> newControl -> IxWAG RunAudio RunEngine newProof Unit { | newGraph } { | newGraph } newControl) ->
+  WAG RunAudio RunEngine proof Unit { | Graph } Control ->
   Scene Extern RunAudio RunEngine proof Unit
 cont' update loop w =
   let
-    IxWAG ix = update (extract w) 
+    IxWAG ix = Ix.do
+      ipatch
+      update (extract w)
   in
-    ibranch (\e a ->
-        if e.active then case e.trigger of
+    ibranch
+      ( \e a ->
+          if e.active then case e.trigger of
             InitialEvent -> Right (loop e a)
             HotReload (Wag bam) -> Left bam
-        else
-          Right (loop e a)) (ix w)
+          else
+            Right (loop e a)
+      )
+      (ix w)
 
 cont ::
   forall newGraph newControl proof.
   GraphIsRenderable newGraph =>
-  (Control -> IxWAG RunAudio RunEngine proof Unit Graph {|newGraph} newControl) ->
-  (forall newProof.
+  Patch Graph newGraph =>
+  (Control -> IxWAG RunAudio RunEngine proof Unit { | newGraph } { | newGraph } newControl) ->
+  ( forall newProof.
     Extern ->
     newControl ->
-    IxWAG RunAudio RunEngine newProof Unit {|newGraph} {|newGraph} newControl
+    IxWAG RunAudio RunEngine newProof Unit { | newGraph } { | newGraph } newControl
   ) ->
   Wag
-cont update loop = Wag (unsafeCoerce (cont' update loop))
+cont update loop = Wag (unsafeCoerce go)
+  where
+  go :: WAG RunAudio RunEngine proof Unit { | Graph } Control -> Scene Extern RunAudio RunEngine proof Unit
+  go = cont' update loop
