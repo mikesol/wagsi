b93ee6fef5a5b609b65fdfd078c85f4e8d9d5119
Mike Solomon <mike@meeshkan.com>
2021-06-21 21:51:46 +0300

    Updates purescript

--- src/Wagged.purs
+++ src/Wagged.purs
@@ -12,5 +12,8 @@ control _ (a :: Unit) = a
 graph e (a :: Unit) =
   a
     /\ speaker
-        { unit0: gain (cos (pi * e.time) * -0.02 + 0.02) { osc0: sinOsc 440.0 }
+        { unit0: gain (cos (pi * e.time) * -0.02 + 0.02) { osc0: sinOsc (sin (pi * e.time * 10.0) * 5.0 + 440.0) }
+        , unit1: gain (cos (pi * e.time * 1.2) * -0.02 + 0.02) { osc1: sinOsc (sin (pi * e.time * 2.0) * 5.0 + 660.0) }
+        , dline: gain 1.0 { unit2: ref, delay: delay (0.6 + sin (e.time * pi * 0.3) * 0.1)  { dgain: gain 0.4 { dline: ref } } }
+        , unit2: gain 0.2 { lb0: loopBuf { playbackRate: 1.1 + sin (pi * e.time * 0.4) * 0.4 } "drop" }
         }
