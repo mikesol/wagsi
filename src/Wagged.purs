2a561c4099b5aed4a4a6f26e59262e40e87f151f
Mike Solomon <mike@meeshkan.com>
2021-06-21 15:32:29 +0300

    Adds annotations

--- src/Wagged.purs
+++ src/Wagged.purs
@@ -7,11 +7,10 @@ import WAGS.Change (ichange)
 import WAGS.Create.Optionals
 import WAGS.Graph.AudioUnit (OnOff(..), TGain, TSinOsc, TSpeaker)
 
-control _ a = a
+control _ (a :: Unit) = a
 
-graph e a =
+graph e (a :: Unit) =
   a
     /\ speaker
-        { unit0: gain (sin (e.time * pi * 3.0) * 0.04 + 0.09) 
-          { osc0: sinOsc (if e.time % 2.0 < 1.4 then 337.0 + sin (pi * e.time * 2.0) * 10.0 else 440.0) }
+        { unit0: gain (cos (pi * e.time) * -0.02 + 0.02) { osc0: 440.0 }
         }
