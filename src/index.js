ee8482c66c0e953a84cd5421d2a346c67cd8ce4a
Mike Solomon <mike@meeshkan.com>
2021-06-21 10:57:27 +0300

    Preparing for the gopher

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
 import main from "../output/Main/index.js";
-import psci from "../output/Wagged/index.js";
+import psci from "../output/Gopher/index.js";
 import hack from "../output/Hack/foreign.js";
 main.main();
 
 if (module.hot) {
-  module.hot.accept("../output/Wagged/index.js", function() {
-    console.log('Accepting the updated module!', psci.it);
+  module.hot.accept("../output/Gopher/index.js", function() {
+    console.log('Hello wags!', psci.it);
     var handlers = hack.handlers();
     for (var item in handlers) {
       handlers[item](psci.it)();
