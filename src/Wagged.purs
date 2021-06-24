073a734e07135c596a228314140b000a01e5aaf5
Mike Solomon <mike@meeshkan.com>
2021-06-24 12:10:39 +0000

    Brings back previous graph

--- src/Wagged.purs
+++ src/Wagged.purs
@@ -13,9 +13,4 @@ control (_:: Extern) (a :: Unit) = a
 -- change this to make sound
 -- for example, you can try:
 -- a /-\ speaker { unit0: gain (cos (pi * e.time) * -0.02 + 0.02) { osc0: sinOsc 440.0 } }
-graph (e :: Extern) (a :: Unit) = a /-\ speaker { unit0: gain (cos (pi * e.time) * -0.03 + 0.03) { osc0: sinOsc 440.0 },
-
-unit1: gain (cos (pi * e.time) * -0.03 + 0.03) { osc1: sinOsc 660.0 },
-unit2: gain (cos (pi * e.time* 0.99) * -0.03 + 0.03) { osc2: sinOsc 1120.0 },
-unit3: gain (cos (pi * e.time * 1.02) * -0.015 + 0.015) { osc3: sinOsc 1900.0 }
- }
+graph (e :: Extern) (a :: Unit) = a /-\ speaker { toSpeaker: constant 0.0 }
