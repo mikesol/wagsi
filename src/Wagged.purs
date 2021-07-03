71e28f262454bc49f0bfc00041222729a55529b0
Mike Solomon <mike@meeshkan.com>
2021-07-03 23:12:00 +0300

    Messing around

--- src/Wagged.purs
+++ src/Wagged.purs
@@ -4,21 +4,29 @@ import Math
 import Prelude
 import WAGS.Create.Optionals
 
+import Control.Comonad.Cofree (head, tail)
+import Data.Int (toNumber)
+import Data.Newtype (unwrap)
 import Data.Tuple.Nested (type (/\), (/\))
 import EZCtrl (ezctrl)
-import FromEnv (ORow)
-import Wagsi.Types (Extern)
+import FromEnv (ORow(..))
 import Hack ((/@\))
+import LibWrap (ARate(..))
 import WAGS.Change (ichange)
 import WAGS.Graph.AudioUnit (OnOff(..), TGain, TSinOsc, TSpeaker)
+import Wagsi.Types (Extern)
 
-control :: Extern -> ORow () -> ORow ()
+control :: Extern -> ORow (rate0 :: ARate) -> ORow (rate0 :: ARate)
 control = ezctrl
 
 -- change this to make sound
 -- for example, you can try:
 -- a /\ speaker { unit0: gain (cos (pi * e.time) * -0.02 + 0.02) { osc0: sinOsc 440.0 } }
 
-graph (e :: Extern) (a :: ORow ()) = a /\ speaker { toSpeaker: constant 0.0 }
+graph ({ time, headroom } :: Extern) (ORow a :: ORow (rate0 :: ARate)) =
+  let
+    rate = unwrap a.rate0 { time,  rate: 8.0 + sin (pi * time) * 6.0 }
+  in
+    (ORow {rate0: (ARate (tail rate))}) /\ speaker { unit0: gain (cos (pi * (head rate)) * -0.02 + 0.02) { osc0: sinOsc 440.0 } }
 
 wagsi = control /@\ graph
