1c2f3d4bd65997a5821380ce6ec35bfe9a688ea7
Mike Solomon <mike@meeshkan.com>
2021-07-05 17:44:26 +0300

    lowwww

--- src/Wagged.purs
+++ src/Wagged.purs
@@ -53,7 +53,7 @@ wagsi ({ time, headroom: headroom' } :: Extern) (a :: Acc) =
             gain 1.0
               { oscUnit0Player0:
                   gain (ff 0.04 (pure (sin (pi * time * 3.0) * 0.03 + 0.02)))
-                    { osc0Player0: sinOsc (440.0 + sin (pi * time) * 10.0) }
+                    { osc0Player0: sinOsc (90.0 + sin (pi * time) * 6.0) }
               , bufUnit0Player0:
                   gain 1.0
                     { bufUnit0G0Player0:
