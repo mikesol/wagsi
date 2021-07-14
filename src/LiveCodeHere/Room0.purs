7303e672767635973c88438ca78e184179276a91
Mike Solomon <mike@meeshkan.com>
2021-07-14 08:51:26 +0300

    Initial work

--- src/LiveCodeHere/Room0.purs
+++ src/LiveCodeHere/Room0.purs
@@ -3,11 +3,17 @@ module WAGSI.LiveCodeHere.Room0 where
 import Prelude
 import Math
 import WAGS.Create.Optionals
-
 import WAGS.Lib.Rate (ARate)
 import WAGSI.Plumbing.Types (Extern)
 
-type Acc (r :: Row Type) = ( room0Rate0 :: ARate | r )
+data MidiInstr
+  = NoteOn Int Int
+  | NoteOff Int
+
+notes ={}
+
+type Acc r
+  = ( room0Rate0 :: ARate | r )
 
 actualizer :: Extern -> { room0Rate0 :: Number }
 actualizer _ = { room0Rate0: 1.0 }
