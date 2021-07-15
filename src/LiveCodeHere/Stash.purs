4e8605f021b59d61bc6529321c99c48182d5138e
Mike Solomon <mike@meeshkan.com>
2021-07-15 12:48:52 +0300

    Adds actualizer

--- src/LiveCodeHere/Stash.purs
+++ src/LiveCodeHere/Stash.purs
@@ -5,7 +5,20 @@ import WAGSI.Plumbing.FFIStuff (Stash, stashable)
 stash :: Stash
 stash =
   stashable
-    { buffers: {}
+    { buffers:
+        { kick1: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/kick1.mp3"
+        , sideStick: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/sideStick.mp3"
+        , snare: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/snare.mp3"
+        , clap: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/clap.mp3"
+        , snareRoll: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/snareRoll.mp3"
+        , kick2: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/kick2.mp3"
+        , closedHH: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/closedHH.mp3"
+        , shaker: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/shaker.mp3"
+        , openHH: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/openHH.mp3"
+        , tamb: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/tamb.mp3"
+        , crash: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/crash.mp3"
+        , ride: "https://meeshkan-public-assets.s3.eu-west-1.amazonaws.com/wags/smplxmpl/ride.mp3"
+        }
     , periodicWaves: {}
     , floatArrays: {}
     }
