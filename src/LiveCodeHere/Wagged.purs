3ddf9ce0ba8ec2a096f83d024fd812bcc3e0b5a2
Mike Solomon <mike@meeshkan.com>
2021-09-28 16:50:43 +0300

    Adds more tests

--- src/LiveCodeHere/Wagged.purs
+++ src/LiveCodeHere/Wagged.purs
@@ -1,27 +1,6 @@
 module WAGSI.LiveCodeHere.Wagged where
 
-import Prelude
-
-import Data.Lens (_Just, over, set, traversed)
-import Data.Lens.Iso.Newtype (unto)
-import Math (pi, cos)
-import WAGSI.Plumbing.Tidal (NoteInFlattenedTime(..), TheFuture, c2s, lnr, lnv, make, onTag, parse, s, s2f)
-
-trigfun :: Number -> Number
-trigfun x
-  | x < 0.5 = x
-  | otherwise = cos (pi * 2.0 * (x - 0.5)) * 0.25 / (-1.0) + 0.75
+import WAGSI.Plumbing.Tidal (TheFuture, make, s)
 
 wag :: TheFuture
-wag = make 2.0
-  { earth: s
-      $ map
-          ( over (traversed <<< unto NoteInFlattenedTime)
-              (\i -> i { bigStartsAt = trigfun (i.bigStartsAt / i.bigCycleLength) * i.bigCycleLength }) <<< s2f
-          )
-      $ c2s
-      $ parse " bass:3 blip*4 "
-  , wind: s $ onTag "x" (set (_Just <<< lnv) (const 0.3))
-      $ onTag "x" (set (_Just <<< lnr) (\{ normalizedBigCycleTime } ->  0.7 + (normalizedBigCycleTime * 0.84)))
-      $ parse "~ blip*4;x"
-  }
+wag = make 1.0 { earth: s "" }
