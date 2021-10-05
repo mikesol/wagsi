92327959f27edfcaf75c83663520296f5a66ab90
Mike Solomon <mike@meeshkan.com>
2021-10-05 11:42:40 +0300

    Uses lfo

--- src/Plumbing/Example.purs
+++ src/Plumbing/Example.purs
@@ -5,7 +5,6 @@ import Prelude
 import Data.Lens (_Just, set)
 import Data.Maybe (Maybe(..))
 import Data.Profunctor (lcmap)
-import Math (pi, sin)
 import WAGS.Create.Optionals (highpass)
 import WAGSI.Plumbing.Cycle (c2d, lowdark)
 import WAGSI.Plumbing.Download (HasOrLacks(..))
@@ -13,9 +12,10 @@ import WAGSI.Plumbing.FX (fx, goodbye, hello)
 import WAGSI.Plumbing.Samples (clockTime)
 import WAGSI.Plumbing.Tidal (ldt, make, s)
 import WAGSI.Plumbing.Types (TheFuture)
+import Wags.Learn.Oscillator (lfo)
 
 title = "High-pass filter" :: String
-hasOrLacks = (Just $ Has ["tabla_0", "chin_0", "tabla_4", "tabla_6", "lowdark_0"]) :: Maybe HasOrLacks
+hasOrLacks = (Just $ Has [ "tabla_0", "chin_0", "tabla_4", "tabla_6", "lowdark_0" ]) :: Maybe HasOrLacks
 
 example :: TheFuture
 example = make 1.0
@@ -24,7 +24,7 @@ example = make 1.0
       set (_Just <<< ldt)
         ( lcmap clockTime \t -> fx
             ( goodbye $ highpass
-                { freq: 3010.0 + sin (pi * t * 0.1) * 3000.0
+                { freq: 3010.0 + lfo { phase: 0.0, freq: 0.1, amp: 3000.0 } t
                 , q: 1.0
                 }
                 hello
