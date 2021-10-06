2385c20b6b5f1ab099ada805fcbf1b63435a7251
Mike Solomon <mike@meeshkan.com>
2021-10-06 11:05:14 +0300

    Adds poly

---
+++ cookbook/PollyWannaCracker.purs
@@ -0,0 +1,18 @@
+module WAGSI.Cookbook.PollyWannaCracker where
+
+import Prelude
+
+import Data.Lens (_Just, set)
+import Data.Profunctor (lcmap)
+import WAGSI.Plumbing.Cycle (c2d, lowdark)
+import WAGSI.Plumbing.Samples (normalizedSampleTime)
+import WAGSI.Plumbing.Tidal (lnr, lnv, make, parse, s)
+import WAGSI.Plumbing.Types (TheFuture)
+
+wag :: TheFuture
+wag = make 1.0
+  { earth: s $ map (set (_Just <<< lnr) (lcmap normalizedSampleTime \t -> 1.0)) $ parse "newnotes newnotes:1 newnotes:2 newnotes:3"
+  -- comment `wind` in and out
+  , wind: s $ map (set (_Just <<< lnv) (const 0.1) <<< set (_Just <<< lnr) (lcmap normalizedSampleTime \t -> 2.0 {- -t -} )) $ parse "newnotes newnotes:1 newnotes:2 newnotes:3 newnotes:6"
+  , heart: c2d lowdark
+  }
