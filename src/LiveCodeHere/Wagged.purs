ae5a7d43aa3bd9e0fe2fbca082ecc0e88439d588
Mike Solomon <mike@meeshkan.com>
2021-10-11 07:58:15 +0300

    Resets to blank document

--- src/LiveCodeHere/Wagged.purs
+++ src/LiveCodeHere/Wagged.purs
@@ -1,24 +1,10 @@
 module WAGSI.LiveCodeHere.Wagged where
 
-import Prelude
-
-import Data.Lens (set)
-import Data.Profunctor (lcmap)
-import WAGSI.Plumbing.Samples (initialEntropy)
-import WAGSI.Plumbing.Samples as S
-import WAGSI.Plumbing.Tidal (lns, lnv, make, onTag, parse, s)
+import WAGSI.Plumbing.Tidal (make, s)
 import WAGSI.Plumbing.Types (TheFuture)
 
 wag :: TheFuture
 wag = make 1.0
-  { earth: s
-      $ onTag "tb"
-          ( map
-              $ compose
-                  (set lnv $ lcmap initialEntropy ((>) 0.5 >>> if _ then 0.0 else 1.0))
-                  (set lns $ lcmap initialEntropy ((>) 0.5 >>> if _ then S.tabla_0__Sample else S.tabla2_0__Sample))
-          )
-      $ parse "bassdm <chin*4 psr> tabla;tb ~"
-  , title: "Tabla + glitch"
-  , preload: [S.tabla_0__Sample, S.tabla2_0__Sample]
+  { earth: s ""
+  , title: "i m a k e n o i s e"
   }
