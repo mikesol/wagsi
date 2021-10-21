25ac9eabe91a009779f712a7b3f40b7b39cee06d
Mike Solomon <mike@meeshkan.com>
2021-10-21 13:06:52 +0300

    Adds simple groove

---
+++ cookbook/SimpleGroove.purs
@@ -0,0 +1,15 @@
+module WAGSI.Cookbook.SimpleGroove where
+
+import Prelude
+
+import WAGSI.Plumbing.Tidal (make, s)
+import WAGSI.Plumbing.Types (TheFuture)
+
+m2 = 4.0 * 1.0 * 60.0/111.0 :: Number
+
+wag :: TheFuture
+wag =
+  make (m2 * 2.0)
+    { earth: s "bd:3 _ hh _ bd:6 bd hh ~ "
+    , title: "i m a k e n o i s e"
+    }
