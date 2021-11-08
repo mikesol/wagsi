c444b59bf31bd2ee6f9e2dfbde13f68b472b05c0
Mike Solomon <mike@meeshkan.com>
2021-11-08 15:43:48 +0200

    Bring back default live env

--- src/LiveCodeHere/Wagged.purs
+++ src/LiveCodeHere/Wagged.purs
@@ -1,21 +1,11 @@
 module WAGSI.LiveCodeHere.Wagged where
 
-import Prelude
+import WAGSI.Plumbing.Tidal (make, s)
+import WAGSI.Plumbing.Types (TheFuture)
 
-import Data.Lens (_Just, set, traversed)
-import Data.Newtype (unwrap)
-import Data.Profunctor (lcmap)
-import WAGS.Lib.Tidal.Tidal (lnr, make, parse_, s)
-import WAGSI.Plumbing.Types (WhatsNext)
-import WAGSI.Sounds.Gamelan as Gamelan
-
-wag :: WhatsNext
-wag = make 2.0
-  { earth: s 
-      $ set (traversed <<< _Just <<< lnr) (lcmap unwrap \{ normalizedBigCycleTime  } -> 1.0 + normalizedBigCycleTime * 0.0)
-      $ parse_ """
-      BBPL1 BBPL2h BBPL2h BBPL3 BBPL2h BBPL2h BBPL5 BBPL2h BBPL2h BBPL4 BBPL6 BBPL6 BBPL3h BBPL2h BBPL4 BBPL2h
-      """
-  , title: "gamelan 1"
-  , sounds: Gamelan.sounds
-  }
+wag :: TheFuture
+wag =
+  make 1.0
+    { earth: s ""
+    , title: "i m a k e n o i s e"
+    }
