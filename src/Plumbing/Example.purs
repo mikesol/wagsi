0c7c2f0ea53009b73f59651e23860bb7b39959b9
Mike Solomon <mike@meeshkan.com>
2021-11-11 17:26:45 +0200

    Uses main

--- src/Plumbing/Example.purs
+++ src/Plumbing/Example.purs
@@ -2,11 +2,11 @@ module WAGSI.Plumbing.Example where
 
 import Prelude
 
+import Data.List ((:), List(..))
+import Data.Tuple.Nested ((/\))
 import WAGS.Lib.Tidal.Tidal (make, s)
-import WAGS.Lib.Tidal.Types (Sample(..))
+import WAGSI.Plumbing.Guards (asFofTime)
 import WAGSI.Plumbing.Types (WhatsNext)
 
 wag :: { clockTime :: Number } -> WhatsNext
-wag { clockTime }
-  | clockTime < 3.2 =  make 1.0 { earth: s $ "bd:0 bd:0", preload: map Sample ["bd:0", "hh:0", "chin:0"] }
-  | otherwise =  make 1.0 { earth: s $ "bd:0 bd:0 hh:0 chin:0*4" }
+wag = asFofTime ((4.0 /\ make 1.0 { earth: s $ "bd:0 bd:0" } ): Nil) (make 1.0 { earth: s $ "bd:0 bd:0 hh:0 chin:0*4" })
