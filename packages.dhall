3335e9f21ae650932a0dbfd4c902cfa34b9881e6
Mike Solomon <mike@wavr.so>
2021-11-25 18:29:17 +0200

    Bumps version

--- packages.dhall
+++ packages.dhall
@@ -231,7 +231,7 @@ let additions =
           , "strings"
           ]
         , repo = "https://github.com/mikesol/purescript-wags-lib.git"
-        , version = "v0.0.59"
+        , version = "v0.0.60"
         }
       , painting =
         { dependencies =
--- src/LiveCodeHere/Wagged.purs
+++ src/LiveCodeHere/Wagged.purs
@@ -5,17 +5,14 @@ import Prelude
 import Data.Lens (_Just, set, traversed)
 import Data.Newtype (unwrap)
 import Data.Profunctor (lcmap)
-import WAGS.Lib.Tidal.Tidal (lnr, make, parse_, s)
-import WAGSI.Plumbing.Types (WhatsNext)
 import WAGS.Lib.Sounds.Gamelan as Gamelan
+import WAGS.Lib.Tidal (AFuture)
+import WAGS.Lib.Tidal.Tidal (lnr, make, parse_, s)
 
-wag :: WhatsNext
-wag = make 2.0
-  { earth: s
-      $ set (traversed <<< _Just <<< lnr) (lcmap unwrap \{ normalizedBigCycleTime  } -> 1.0 + normalizedBigCycleTime * 0.0)
-      $ parse_ """
-      hh hh hh
-      """
+wag :: AFuture
+wag = make 4.0
+  { earth: s "BBPL1 BBPL2h [BBPL2h BBPL3] BBPL3 BBPL2h [BBPL2h BBPL3] BBPL5 BBPL2h"
+  , wind: s "<[~ DHA*2 ~ LUNG DHA ~ TAK TAK] [[~ DHA*2 ~ DHA] [~ DHUNG TONG TONG]]>"
   , title: "gamelan 1"
   , sounds: Gamelan.sounds
   }
