dd27270f625424f32d44a84fcfdd6528bda91d3d
Mike Solomon <mike@wavr.so>
2021-11-25 16:37:44 +0200

    0.0.59

--- packages.dhall
+++ packages.dhall
@@ -231,7 +231,7 @@ let additions =
           , "strings"
           ]
         , repo = "https://github.com/mikesol/purescript-wags-lib.git"
-        , version = "v0.0.58"
+        , version = "v0.0.59"
         }
       , painting =
         { dependencies =
--- src/LiveCodeHere/Wagged.purs
+++ src/LiveCodeHere/Wagged.purs
@@ -1,11 +1,21 @@
 module WAGSI.LiveCodeHere.Wagged where
 
-import WAGS.Lib.Tidal.Tidal (make, s)
+import Prelude
+
+import Data.Lens (_Just, set, traversed)
+import Data.Newtype (unwrap)
+import Data.Profunctor (lcmap)
+import WAGS.Lib.Tidal.Tidal (lnr, make, parse_, s)
 import WAGSI.Plumbing.Types (WhatsNext)
+import WAGS.Lib.Sounds.Gamelan as Gamelan
 
 wag :: WhatsNext
-wag =
-  make 1.0
-    { earth: s "[[hh hh]]"
-    , title: "i m a k e n o i s e"
-    }
+wag = make 2.0
+  { earth: s
+      $ set (traversed <<< _Just <<< lnr) (lcmap unwrap \{ normalizedBigCycleTime  } -> 1.0 + normalizedBigCycleTime * 0.0)
+      $ parse_ """
+      hh hh hh
+      """
+  , title: "gamelan 1"
+  , sounds: Gamelan.sounds
+  }
