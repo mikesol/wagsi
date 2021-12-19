bc7cc0704191c72b2c396f14655e115b8cc26438
Mike Solomon <mike@wavr.so>
2021-12-19 10:25:41 -0800

    Osc

--- cookbook/Oscs.purs
+++ cookbook/Oscs.purs
@@ -2,22 +2,42 @@ module WAGSI.Cookbook.Oscs where
 
 import Prelude
 
-import WAGS.Create.Optionals (bandpass, gain, highpass, ref, sawtoothOsc, triangleOsc)
+import Data.Array (replicate)
+import Data.Foldable (foldl)
+import Data.Lens (set, traversed)
+import Data.NonEmpty ((:|))
+import WAGS.Create.Optionals (bandpass, gain, highpass, pan)
 import WAGS.Lib.Learn.Oscillator (lfo)
 import WAGS.Lib.Tidal (AFuture)
+import WAGS.Lib.Tidal.Cycle as C
 import WAGS.Lib.Tidal.FX (fx, goodbye, hello)
-import WAGS.Lib.Tidal.Tidal (addEffect, make, s)
+import WAGS.Lib.Tidal.Tidal (addEffect, b_, i_, lnf, make, s)
 
 fund = 256.0
 
+seq0 = C.alphabet_10 :| [ C.alphabet_13, C.alphabet_14, C.alphabet_2, C.alphabet_1, C.alphabet_7, C.alphabet_9, C.alphabet_20 ]
+
+seq1 = C.alphabet_11 :| [ C.alphabet_16, C.alphabet_15, C.alphabet_3, C.alphabet_4, C.alphabet_8 ]
+
+seq2 = C.alphabet_9 :| [ C.alphabet_12, C.alphabet_14, C.alphabet_0, C.alphabet_25, C.alphabet_6, C.alphabet_22, C.alphabet_23, C.alphabet_24, C.alphabet_17, C.alphabet_18, C.alphabet_19 ]
+
+f n s = b_ (i_ <^> s) [i_ <^> (foldl (<>) s (replicate n s))]
+seq00 = f 1 seq0
+seq11 = f 2 seq1
+seq22 = f 3 seq2
+
+napply f (h :| t) = f h t
+
+infixr 5 napply as <^>
+
 wag :: AFuture
 wag =
-  make 8.0
+  make 4.6
     { earth:
         map
           ( addEffect
               \{ clockTime } ->
-                fx $ goodbye $ gain 1.0
+                fx $ goodbye $ gain 0.02
                   { ipt: hello
                   , mxr: gain 1.0
                       { bp0: bandpass
@@ -29,24 +49,47 @@ wag =
                               clockTime
                           , q: 40.0
                           }
-                          { oscs: ref }
-                      , bp1: highpass
-                          { freq: 1500.0 + lfo
-                              { phase: 0.0
-                              , freq: 0.1
-                              , amp: 1400.0
-                              }
-                              clockTime
-                          , q: 30.0
-                          }
-                          { oscs: ref }
-                      , muted: gain 0.0
-                          { oscs: gain 1.0
-                              { osc0: sawtoothOsc fund
-                              , osc1: triangleOsc (fund * 2.0 + 10.0)
-                              }
-                          }
+                          hello
                       }
                   }
-          ) $ s $ "tink:1 tink:2 tink:3 tink:4"
+          ) $ s $ seq00
+    , wind:
+        map
+          ( addEffect
+              \{ clockTime } ->
+                fx $ goodbye $ gain 0.03
+                  { ipt: hello
+                  , mxr: gain 1.0
+                      { bp0: pan
+                          ( lfo
+                              { phase: 0.0
+                              , freq: 3.0
+                              , amp: 0.8
+                              }
+                              clockTime
+                          )
+                          hello
+                      }
+                  }
+          ) $ s $ seq11
+    , fire:
+        map
+          ( addEffect
+              \{ clockTime } ->
+                fx $ goodbye $ gain 0.05
+                  { ipt: hello
+                  , mxr: gain 1.0
+                      { bp0: highpass
+                          { freq: 2000.0 + lfo
+                              { phase: 0.0
+                              , freq: 0.15
+                              , amp: 1500.0
+                              }
+                              clockTime
+                          , q: 10.0
+                          }
+                          hello
+                      }
+                  }
+          ) $ s $ (set (traversed <<< traversed <<< lnf) false) seq22
     }
---
+++ z.py
@@ -0,0 +1,4 @@
+for x in range(32):
+  print("glitch:%d " % (x % 7),end="")
+  if x % 8 == 7:
+    print("")
