eaf571149f3c7eda57ffd4022352f0be5331dbc2
Mike Solomon <mike@wavr.so>
2021-12-26 06:23:55 +0200

    Bumps version of wags-lib

--- cookbook/Smgrp.purs
+++ cookbook/Smgrp.purs
@@ -14,7 +14,7 @@ wag0 =
 
 wag1 :: AFuture
 wag1 =
-  make 1.0
+  make 0.7
     { earth: s "notes:5 notes:6 notes:7 notes:8"
     , title: "notes1"
     }
@@ -22,14 +22,14 @@ wag1 =
 
 wag2 :: AFuture
 wag2 =
-  make 1.0
+  make 1.3
     { earth: s "notes:9 notes:10 notes:9 notes:10"
     , title: "notes2"
     }
 
 wag3 :: AFuture
 wag3 =
-  make 1.0
+  make 1.1
     { earth: s "notes:11 notes:12 notes:11 notes:12"
     , title: "notes3"
     }
