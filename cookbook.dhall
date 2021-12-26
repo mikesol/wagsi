e78bcdd510c7f2f78ab61c7c8dd25630ee3a5941
Mike Solomon <mike@wavr.so>
2021-12-26 06:23:12 +0200

    Bumps version of wags-lib

--- cookbook.dhall
+++ cookbook.dhall
@@ -2,5 +2,5 @@ let conf = ./spago.dhall
 
 in      conf
     //  { sources = conf.sources # [ "cookbook/**/*.purs" ]
-        , dependencies = conf.dependencies # [ "strings" ]
+        , dependencies = conf.dependencies # [ "strings", "identity" ]
         }
---
+++ cookbook/Smgrp.purs
@@ -0,0 +1,38 @@
+module WAGSI.Cookbook.Smgrp where
+
+import Prelude
+
+import WAGS.Lib.Tidal (AFuture)
+import WAGS.Lib.Tidal.Tidal (make, s)
+
+wag0 :: AFuture
+wag0 =
+  make 1.0
+    { earth: s "notes:0 notes:1 notes:2 notes:3"
+    , title: "notes0"
+    }
+
+wag1 :: AFuture
+wag1 =
+  make 1.0
+    { earth: s "notes:5 notes:6 notes:7 notes:8"
+    , title: "notes1"
+    }
+
+
+wag2 :: AFuture
+wag2 =
+  make 1.0
+    { earth: s "notes:9 notes:10 notes:9 notes:10"
+    , title: "notes2"
+    }
+
+wag3 :: AFuture
+wag3 =
+  make 1.0
+    { earth: s "notes:11 notes:12 notes:11 notes:12"
+    , title: "notes3"
+    }
+
+wag :: AFuture
+wag = (wag0 <> (wag1 <> wag1)) <> ((wag2 <> wag2) <> wag3)
