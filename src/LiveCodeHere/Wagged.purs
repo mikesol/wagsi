560c7738fd286b98fb3b95e87370f7a9045981f9
Mike Solomon <mike@wavr.so>
2022-01-02 18:35:13 +0200

    Resets wagged

--- src/LiveCodeHere/Wagged.purs
+++ src/LiveCodeHere/Wagged.purs
@@ -5,31 +5,8 @@ import WAGS.Lib.Tidal.Tidal (make, s)
 
 wag :: AFuture
 wag =
-  make 0.6
-    { earth: s """<
-  [bd bd hh*2 [notes:6, chin*4]]
-  [bd bd hh*2 [notes:7, chin*4]]
-  [bd bd hh*2 [notes:8, chin*4]]
-  [psr:2 hh hh hh*2]
-  [bd bd hh*2 [notes:10, chin*4]]
-  [bd bd hh*3 [notes:12, chin*4]]
-  [bd bd hh*2 [notes:13, chin*4]]
-  [bd [bd,hh:8] hh [notes:14, chin*4]]
-  [bd bd hh*2 [notes:1, chin*4]]
-
-  [[bd,hh:4] bd hh*2 [notes:6, chin*4] bd bd hh*2 [notes:7, chin*4]]
-  [bd bd hh*2 [notes:8, chin*4] bd bd hh*2 [notes:9, chin*4]]
-  [[bd,hh:5] bd hh*2 [notes:10, chin*4] bd bd hh*2 [notes:12, chin*4]]
-  [bd bd hh*2 [notes:13, chin*4] bd bd hh*2 [notes:14, chin*4]]
-
-  [bd bd hh*2 [notes:13, chin*4]]
-  [glitch:4 bd hh*2 [notes:14, chin*4]]
-
-  [[bd,hh:1] bd hh*2 [notes:6, chin*4] bd bd hh*2 [notes:7, chin*4]]
-
-  [bd [bd,hh:4] hh*2 [notes:13, chin*4]]
-  [bd hh*2 [notes:6, chin*4] bd hh*2 [notes:7, chin*4]]
-
->"""
-    , title: "Fast and Furry-ous"
+  make 1.0
+    { earth: s ""
+    , title: "i m a k e n o i s e"
     }
+
