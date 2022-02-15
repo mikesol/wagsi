1af2bc439c477bb58e700b474c828dc433019d39
Mike Solomon <mike@wavr.so>
2022-02-15 07:53:31 +0200

    Adds lesson 10

--- cookbook/CourseLesson10.purs
+++ cookbook/CourseLesson10.purs
@@ -6,6 +6,7 @@ import Data.Maybe (maybe)
 import Data.Newtype (wrap)
 import Foreign.Object (lookup, singleton)
 import WAGS.Create.Optionals (bandpass, highpass, lowpass, convolver, gain)
+import WAGS.Graph.Parameter (ff)
 import WAGS.Lib.Learn.Oscillator (lfo)
 import WAGS.Lib.Tidal.FX (fx, goodbye, hello)
 import WAGS.Lib.Tidal.Tidal (addEffect, make, s)
@@ -24,25 +25,25 @@ wag =
     , wind:
         map
           ( addEffect
-              \{ clockTime } ->
+              \{ clockTime } -> let dl = ff 0.03 <<< pure in
                 fx $ goodbye $ gain 1.0
                   { bp0: lowpass
-                      { freq: 500.0 + lfo { amp: 250.0, freq: 0.1, phase: 0.0 } clockTime
+                      { freq: 500.0 + lfo { amp: 250.0, freq: 0.1, phase: 0.0 } clockTime # dl
                       , q: 10.0
                       }
                       hello
                   , bp1: bandpass
-                      { freq: 1000.0 + lfo { amp: 500.0, freq: 0.3, phase: 0.0 } clockTime
+                      { freq: 1000.0 + lfo { amp: 500.0, freq: 0.3, phase: 0.0 } clockTime # dl
                       , q: 15.0
                       }
                       hello
                   , bp2: bandpass
-                      { freq: 2000.0 + lfo { amp: 1000.0, freq: 0.7, phase: 0.0 } clockTime
+                      { freq: 2000.0 + lfo { amp: 1000.0, freq: 0.7, phase: 0.0 } clockTime # dl
                       , q: 20.0
                       }
                       hello
                   , bp3: highpass
-                      { freq: 3000.0 + lfo { amp: 1500.0, freq: 1.0, phase: 0.0 } clockTime
+                      { freq: 3000.0 + lfo { amp: 1500.0, freq: 1.0, phase: 0.0 } clockTime # dl
                       , q: 25.0
                       }
                       hello
